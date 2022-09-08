bc65c58efea4369e29f255498896f5bfb0d715a3
Danny-2004 <danielegornu@gmail.com>
2022-09-08 13:40:54 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,7 @@
 * main - print the string in the put function
 *
 * Return: Always 0 (Success)
+*
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * return: 0
