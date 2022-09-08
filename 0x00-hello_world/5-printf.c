a14c7980ce213992ac5f0f0aaa6dbb7089a6d158
Danny-2004 <danielegornu@gmail.com>
2022-09-08 08:45:48 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
-* Return: 0
+* Description: using the main fumction
+* this program prints "Programming is like building a multilingual puzzle
+* return: 0
 */
-int main(void)
+int man(void)
 {
-printf("/"with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the output is a piece of art,\n");
 return (0);
 }
